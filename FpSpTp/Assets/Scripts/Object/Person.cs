76e66560fe02e9d72bdc55d7b16092f7e3552dd7
Nikita Kondibor <nikitakondibor@gmail.com>
2023-04-30 17:36:55 +0300

    add energy

--- FpSpTp/Assets/Scripts/Object/Person.cs
+++ FpSpTp/Assets/Scripts/Object/Person.cs
@@ -6,8 +6,13 @@ using UnityEngine;
 
 public class Person : Object
 {
+    private int _energy; // if energy is low person cant doing anything
+    private int _energy_waste_per_action = 5; 
+    private int _energy_gain_per_stroke = 1;
+    private int _start_energy = 50;
+    
     private int _affection; // if affection = 0, person is disappear
-    private int _start_affection = 50;
+    private int _start_affection = 50; 
     private int _lose_affection_per_time = 1;
 
     public void Init()
@@ -16,14 +21,38 @@ public class Person : Object
         GetComponent<SpriteRenderer>().sprite = downArt;
         _position = new Vector3(0, 0, 0);
         _affection = _start_affection;
+        _energy = _start_affection;
+    }
+
+    public void Init(float x, float y, float z)
+    {
+        _type = ObjectType.PERSON;
+        GetComponent<SpriteRenderer>().sprite = downArt;
+        _position = new Vector3(x, y, z);
+        _affection = _start_affection;
+        _energy = _start_energy;
+    }
+
+    public void LoseEnergy()
+    {
+        _energy -= _energy_waste_per_action;
+        if(_energy <= 0) Rest();
     }
 
     public void Move(float dx, float dy)
     {
-        _position.x += dx;
-        _position.y += dy;
-        transform.position = _position;
-        UpdateArt(dx, dy);
+        if (_energy >= _energy_waste_per_action)
+        {
+            _position.x += dx;
+            _position.y += dy;
+            transform.position = _position;
+            UpdateArt(dx, dy);
+            LoseEnergy();
+        }
+        else
+        {
+            Rest();
+        }
     }
 
     private void UpdateArt(float dx, float dy)
@@ -42,14 +71,14 @@ public class Person : Object
         {
             GetComponent<SpriteRenderer>().sprite = downArt;
         }
-     
     }
 
-    public void Init(float x, float y, float z)
+    public int GetEnergy()
     {
-        _position = new Vector3(x, y, z);
-        _affection = _start_affection;
+        return _energy;
     }
+
+    public void SetEnergy(int newenergy) => _energy = newenergy; 
 
     public int GetAffection()
     {
@@ -61,6 +90,11 @@ public class Person : Object
     public void LosePerson()
     {
         // person disappear
+    }
+
+    public void Rest()  // if energy <= energy_waste_per_action;
+    {
+        _energy += _energy_gain_per_stroke;
     }
 
     public void LoseAffection()
