5ee853bacefad86c386e80a16ae5c714a6810196
Akel <akulenko.slava@gmail.com>
2023-04-30 18:23:57 +0300

    Поправил камень

--- FpSpTp/Assets/Scripts/Object/Stone.cs
+++ FpSpTp/Assets/Scripts/Object/Stone.cs
@@ -6,15 +6,9 @@ public class Stone : MonoBehaviour
 {
     public Sprite[] stones;
 
-
     private void Start()
     {
         GetComponent<SpriteRenderer>().sprite = stones[Random.Range(0, stones.Length)];
     }
 
-    public void Init(float x, float y)
-    {
-        _type = ObjectType.STONE;
-        _position = new Vector3(x, y, 0);
-    }
 }
